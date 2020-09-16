c01eaac8eeecae761d946794c98b7cb9357e0d39
SergeyRazgulyaev <razgulyaev.sergey@gmail.com>
2020-09-17 00:51:19 +0300

    Lesson 01 Home Work With Post And Photo Cell Prototype
    Lesson 01 Home Work With Post And Photo Cell Prototype

---
+++ Lesson01HomeWork/VKClient_RazgulyaevSergey/NewsScreen/NewsPhotoCell/NewsPhotoCell.swift
@@ -0,0 +1,39 @@
+//
+//  NewsPhotoCell.swift
+//  VKClient_RazgulyaevSergey
+//
+//  Created by Sergey Razgulyaev on 16.09.2020.
+//  Copyright © 2020 Sergey Razgulyaev. All rights reserved.
+//
+
+import UIKit
+
+class NewsPhotoCell: UITableViewCell {
+        @IBOutlet weak var newsAuthorNameLabel: UILabel!
+        @IBOutlet weak var newsDateLabel: UILabel!
+        @IBOutlet weak var newsForMeAvatarView: UIView!
+        @IBOutlet weak var newsForMeAvatarImageView: UIImageView!
+        @IBOutlet weak var newsForMeImageView: UIImageView!
+        @IBOutlet weak var newsLikeIndicator: NewsLike!
+        @IBOutlet weak var newsCommentIndicator: NewsComment!
+        @IBOutlet weak var newsShareIndicator: NewsShare!
+        @IBOutlet weak var newsViewsIndicator: NewsViews!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        newsForMeAvatarView.layer.cornerRadius = newsForMeAvatarView.bounds.width / 2
+        newsForMeAvatarView.layer.shadowColor = UIColor.black.cgColor
+        newsForMeAvatarView.layer.shadowRadius = 5
+        newsForMeAvatarView.layer.shadowOpacity = 1
+        newsForMeAvatarView.layer.shadowOffset = .zero
+        newsForMeAvatarView.layer.shadowPath = UIBezierPath(ovalIn: newsForMeAvatarView.bounds).cgPath
+        
+        newsForMeAvatarImageView.layer.cornerRadius = newsForMeAvatarImageView.bounds.width / 2
+        newsForMeAvatarImageView.clipsToBounds = true
+    }
+}
