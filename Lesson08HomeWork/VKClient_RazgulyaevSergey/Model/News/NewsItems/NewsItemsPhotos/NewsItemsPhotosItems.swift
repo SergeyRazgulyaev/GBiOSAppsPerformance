d3537fa3c5cc3376abd7760fc88d4b64503ac745
SergeyRazgulyaev <razgulyaev.sergey@gmail.com>
2020-10-16 17:08:18 +0300

    Lesson 08 Home Work Final Version
    Lesson 08 Home Work Final Version

---
+++ Lesson08HomeWork/VKClient_RazgulyaevSergey/Model/News/NewsItems/NewsItemsPhotos/NewsItemsPhotosItems.swift
@@ -0,0 +1,29 @@
+//
+//  NewsItemsPhotosItems.swift
+//  VKClient_RazgulyaevSergey
+//
+//  Created by Sergey Razgulyaev on 20.09.2020.
+//  Copyright © 2020 Sergey Razgulyaev. All rights reserved.
+//
+
+import Foundation
+
+class NewsItemsPhotosItems: Decodable {
+    var date: Double = 0
+    var id: Int = 0
+    var ownerID: Int = 0
+    var sizes: [NewsItemsPhotosItemsSizes]
+    var likes: NewsItemsPhotosLikes
+    var reposts: NewsItemsPhotosReposts
+    var comments: NewsItemsPhotosComments
+    
+    enum CodingKeys: String, CodingKey {
+        case date
+        case id
+        case ownerID = "owner_id"
+        case sizes
+        case likes
+        case reposts
+        case comments
+    }
+}
---
+++ Lesson08HomeWork/VKClient_RazgulyaevSergey/Services/Operation/ParseFriendsData.swift
@@ -0,0 +1,24 @@
+//
+//  ParseFriendsData.swift
+//  VKClient_RazgulyaevSergey
+//
+//  Created by Sergey Razgulyaev on 24.09.2020.
+//  Copyright © 2020 Sergey Razgulyaev. All rights reserved.
+//
+
+import Foundation
+
+class ParseFriendsData: Operation {
+    private var outputData: [UserItems] = []
+    
+    override func main() {
+        guard let getDataOperation = dependencies.first as? ​GetDataOperation​, let data = getDataOperation.getData() else { return }
+        guard let friends = try? JSONDecoder().decode(User.self, from: data).response?.items else { return }
+        outputData = friends
+    }
+    
+    //MARK: - Access Methods
+    func getOutputData() -> [UserItems] {
+        return outputData
+    }
+}
